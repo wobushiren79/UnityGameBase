acb380c10da3c2c09cb764bf59d45018314c253f
apple <734719526@qq.com>
2018-10-30 15:58:41 +0800

    项目初始化

---
+++ Assets/Scrpit/Common/ProjectConfigInfo.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class ProjectConfigInfo
+{
+    /// <summary>
+    /// 是否打开日志输出
+    /// </summary>
+    public static readonly bool IS_OPEN_LOG_MSG = true;
+}
---
+++ Assets/Scrpit/Utils/ReflexUtil.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+using System.Reflection;
+
+public class ReflexUtil : ScriptableObject
+{
+    /// <summary>
+    /// 根据反射获取所有属性名称
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="classType"></param>
+    /// <returns></returns>
+    public static List<string> GetAllName<T>(T classType)
+    {
+        List<string> listName = new List<string>();
+        Type type = classType.GetType();
+        PropertyInfo[] propertyInfos = type.GetProperties();
+
+        if (propertyInfos == null)
+            return listName;
+
+        int propertyInfoSize = propertyInfos.Length;
+        for (int i = 0; i < propertyInfoSize; i++)
+        {
+            PropertyInfo propertyInfo = propertyInfos[i];
+            listName.Add(propertyInfo.Name);
+        };
+        return listName;
+    }
+    /// <summary>
+    /// 根据反射获取所有属性名称及值
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="classType"></param>
+    /// <returns></returns>
+    public static Dictionary<String, object> GetAllNameAndValue<T>(T classType)
+    {
+        Dictionary<String, object> listData = new Dictionary<string, object>();
+        Type type = classType.GetType();
+        PropertyInfo[] propertyInfos = type.GetProperties();
+
+        if (propertyInfos == null)
+            return listData;
+
+        int propertyInfoSize = propertyInfos.Length;
+        for (int i = 0; i < propertyInfoSize; i++)
+        {
+            PropertyInfo propertyInfo = propertyInfos[i];
+            listData.Add(propertyInfo.Name, propertyInfo.GetValue(classType,null));
+        };
+        return listData;
+    }
+    /// <summary>
+    /// 根据反射 设置值
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="classType"></param>
+    /// <param name="name"></param>
+    /// <param name="value"></param>
+    public static void SetValueByName<T>(T classType,string name,object value)
+    {
+        Type type = classType.GetType();
+        PropertyInfo propertyInfo = type.GetProperty(name);
+
+        if (propertyInfo == null)
+            return;
+        propertyInfo.SetValue(classType,value,null);
+    }
+}
