17311625cf834a43851a66e44880ec4ec885b5e7
apple <734719526@qq.com>
2018-11-08 10:55:30 +0800

    增加steam创意工坊框架

--- Assets/Scrpit/Common/ProjectConfigInfo.cs
+++ Assets/Scrpit/Common/ProjectConfigInfo.cs
@@ -7,4 +7,9 @@ public class ProjectConfigInfo
     /// 是否打开日志输出
     /// </summary>
     public static readonly bool IS_OPEN_LOG_MSG = true;
+
+    /// <summary>
+    /// steamAppId
+    /// </summary>
+    public readonly static string STEAM_APP_ID = "830620";
 }
