c2ec3f08d8945df2f222a3fc3e75c6bc668d0386
Joker Apple <734719526@qq.com>
2020-12-08 13:48:40 +0800

    基础类添加

--- Assets/Scrpits/Base/BaseUIItem.cs
+++ Assets/Scrpits/Base/BaseUIItem.cs
@@ -4,4 +4,12 @@ using UnityEngine;
 public class BaseUIItem<T> : BaseMonoBehaviour where T : BaseUIComponent
 {
     public T uiComponent;
+
+    public virtual void Awake()
+    {
+        AutoLinkUI();
+        AutoLinkHandler();
+        AutoLinkManager();
+    }
+
 }
