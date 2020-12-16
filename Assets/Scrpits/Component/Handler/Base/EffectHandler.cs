f9deaaba3aeb1858713592a80b98e08c62a1d61d
Joker Apple <734719526@qq.com>
2020-12-16 18:38:18 +0800

    粒子效果基础类上传

---
+++ Assets/Scrpits/Component/Handler/Base/EffectHandler.cs
@@ -0,0 +1,43 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections;
+using System;
+
+public class EffectHandler : BaseHandler<EffectManager>
+{
+
+    /// <summary>
+    /// 播放粒子特效
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="effectPosition"></param>
+    public void PlayEffect(string name, Vector3 effectPosition,float delayTime)
+    {
+        GameObject objEffect = manager.CreateEffect(name);
+        if (objEffect == null)
+            return;
+        objEffect.transform.position = effectPosition;
+        ParticleSystem[] listParticleSystem = objEffect.GetComponentsInChildren<ParticleSystem>();
+        for (int i = 0; i < listParticleSystem.Length; i++)
+        {
+            ParticleSystem particleSystem = listParticleSystem[i];
+            ParticleSystem.MainModule psMain = particleSystem.main;
+            psMain.loop = false;
+            //psMain.stopAction = ParticleSystemStopAction.Callback;
+            particleSystem.Play();
+        }
+        StartCoroutine(CoroutineForDelayDestroy(objEffect,delayTime));
+    }
+
+    public void PlayEffect(string name, Vector3 effectPosition)
+    {
+        PlayEffect(name, effectPosition, 5);
+    }
+
+    public IEnumerator CoroutineForDelayDestroy(GameObject objEffect, float delayTime)
+    {
+        yield return new WaitForSeconds(delayTime);
+        Destroy(objEffect);
+    }
+
+}
---
+++ Assets/Scrpits/Component/Manager/Base/EffectManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class EffectManager : BaseManager
+{
+    public Dictionary<string, GameObject> listEffect = new Dictionary<string, GameObject>();
+    protected string resUrl = "Effect/";
+
+    public GameObject CreateEffect(string name)
+    {
+        GameObject objModel = null;
+        if (listEffect.TryGetValue(name, out objModel))
+        {
+            
+        }
+        else
+        {
+            objModel = CreatEffictModel(name);
+        }
+        if (objModel == null)
+            return null;
+        GameObject objEffect = Instantiate(gameObject, objModel);
+        return objEffect;
+    }
+
+    private GameObject CreatEffictModel(string name)
+    {
+        GameObject objEffictModel = Resources.Load<GameObject>(resUrl + name);
+        objEffictModel.name = name;
+        listEffect.Add(name, objEffictModel);
+        return objEffictModel;
+    }
+}
