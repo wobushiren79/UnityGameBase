7066dd6d2e3960edd6d8c8688677f8491dbc7d9b
Joker Apple <734719526@qq.com>
2021-01-17 19:33:44 +0800

    框架修改

--- Assets/Scrpits/Component/Handler/Base/UIHandler.cs
+++ Assets/Scrpits/Component/Handler/Base/UIHandler.cs
@@ -5,11 +5,18 @@ using UnityEngine.UI;
 
 public class UIHandler : BaseUIHandler<UIHandler, UIManager>
 {
-
+    
     protected override void Awake()
     {
         sortingOrder = 1;
         base.Awake();
+        ChangeUIRenderMode(RenderMode.ScreenSpaceCamera);
+    }
+
+    protected void Update()
+    {
+        if (canvas.worldCamera == null)
+            canvas.worldCamera = Camera.main;
     }
 
 }
