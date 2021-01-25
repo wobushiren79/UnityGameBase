52940f2a48f6bbb3369199a1c974f927399feaf2
Joker Apple <734719526@qq.com>
2021-01-25 14:15:02 +0800

    框架修改

--- Assets/Scrpits/Base/BaseUIManager.cs
+++ Assets/Scrpits/Base/BaseUIManager.cs
@@ -15,7 +15,7 @@ public class BaseUIManager : BaseManager
     /// <returns></returns>
     public BaseUIComponent GetOpenUI()
     {
-        for (int i=0;i< uiList.Count;i++)
+        for (int i = 0; i < uiList.Count; i++)
         {
             BaseUIComponent itemUI = uiList[i];
             if (itemUI.gameObject.activeSelf)
@@ -48,7 +48,7 @@ public class BaseUIManager : BaseManager
     /// </summary>
     /// <param name="uiName"></param>
     /// <returns></returns>
-    public BaseUIComponent GetUIByName(string uiName)
+    public T GetUIByName<T>(string uiName) where T : BaseUIComponent
     {
         if (uiList == null || CheckUtil.StringIsNull(uiName))
             return null;
@@ -57,7 +57,7 @@ public class BaseUIManager : BaseManager
             BaseUIComponent itemUI = uiList[i];
             if (itemUI.name.Contains(uiName))
             {
-                return itemUI;
+                return itemUI as T;
             }
         }
         return null;
@@ -68,9 +68,9 @@ public class BaseUIManager : BaseManager
     /// </summary>
     /// <param name="uiEnum"></param>
     /// <returns></returns>
-    public BaseUIComponent GetUI(UIEnum uiEnum)
+    public T GetUI<T>(UIEnum uiEnum) where T : BaseUIComponent
     {
-        return GetUIByName(EnumUtil.GetEnumName(uiEnum));
+        return GetUIByName<T>(EnumUtil.GetEnumName(uiEnum));
     }
 
     /// <summary>
@@ -98,11 +98,11 @@ public class BaseUIManager : BaseManager
     /// 通过UI的名字开启UI
     /// </summary>
     /// <param name="uiName"></param>
-    public BaseUIComponent OpenUIByName(string uiName)
+    public T OpenUIByName<T>(string uiName) where T : BaseUIComponent
     {
         BaseUIComponent uiComponent = null;
         if (CheckUtil.StringIsNull(uiName))
-            return uiComponent;
+            return uiComponent as T;
         bool hasData = false;
         for (int i = 0; i < uiList.Count; i++)
         {
@@ -122,24 +122,25 @@ public class BaseUIManager : BaseManager
             {
                 GameObject objUIComponent = Instantiate(gameObject, uiModel.gameObject);
                 uiComponent = objUIComponent.GetComponent<BaseUIComponent>();
+                uiComponent.OpenUI();
                 uiList.Add(uiComponent);
             }
             else
             {
-                LogUtil.LogError("没有找到指定UI："+ "ui/ui " + uiName);
+                LogUtil.LogError("没有找到指定UI：" + "ui/ui " + uiName);
             }
         }
-        return uiComponent;
+        return uiComponent as T;
     }
 
     /// <summary>
     /// 开启UI
     /// </summary>
     /// <param name="uiEnum"></param>
-    public void OpenUI(UIEnum uiEnum)
+    public T OpenUI<T>(UIEnum uiEnum) where T : BaseUIComponent
     {
         string uiName = EnumUtil.GetEnumName(uiEnum);
-        OpenUIByName(uiName);
+        return OpenUIByName<T>(uiName);
     }
 
 
@@ -178,7 +179,7 @@ public class BaseUIManager : BaseManager
     /// 通过UI的名字开启UI并关闭其他UI
     /// </summary>
     /// <param name="uiName"></param>
-    public BaseUIComponent OpenUIAndCloseOtherByName(string uiName)
+    public T OpenUIAndCloseOtherByName<T>(string uiName) where T : BaseUIComponent
     {
         if (uiList == null || CheckUtil.StringIsNull(uiName))
             return null;
@@ -192,12 +193,12 @@ public class BaseUIManager : BaseManager
                     itemUI.CloseUI();
             }
         }
-        return OpenUIByName(uiName);
+        return OpenUIByName<T>(uiName);
     }
 
-    public BaseUIComponent OpenUIAndCloseOther(UIEnum ui)
+    public T OpenUIAndCloseOther<T>(UIEnum ui) where T : BaseUIComponent
     {
-       return OpenUIAndCloseOtherByName(EnumUtil.GetEnumName(ui));
+        return OpenUIAndCloseOtherByName<T>(EnumUtil.GetEnumName(ui));
     }
 
     /// <summary>
